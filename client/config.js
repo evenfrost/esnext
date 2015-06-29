81f61879cec3d729dc90303804f933ddc7044338
evenfrost <evenfrosty@gmail.com>
2015-06-29 17:48:26 +0300

    Update browser loader files

--- client/config.js
+++ client/config.js
@@ -15,16 +15,24 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.5.1",
-    "babel-runtime": "npm:babel-runtime@5.5.1",
-    "core-js": "npm:core-js@0.9.14",
+    "babel": "npm:babel-core@5.6.15",
+    "babel-runtime": "npm:babel-runtime@5.6.15",
+    "core-js": "npm:core-js@0.9.18",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.5.1": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:babel-runtime@5.6.15": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:core-js@0.9.14": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
