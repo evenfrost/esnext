3efc430b2ea6af3c949618e03be4a255f6b8a8d3
evenfrost <evenfrosty@gmail.com>
2015-08-19 16:38:35 +0300

    Update jspm to 0.16

--- jspm.config.js
+++ jspm.config.js
@@ -15,13 +15,16 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.7.4",
-    "babel-runtime": "npm:babel-runtime@5.7.0",
+    "babel": "npm:babel-core@5.8.22",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@0.9.18",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.7.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
