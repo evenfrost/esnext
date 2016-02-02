dc7840ec3cec8a238dd3058ed6f882d836876ab2
evenfrost <kisolov@gmail.com>
2016-02-02 18:12:39 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app
   .use(convert(etag()))
   .use(convert(error()))
   .use(convert(serve(path.join(__dirname, 'public'))))
-  .use(mount('/jspm_packages', convert(serve(path.join(__dirname, 'jspm_packages')))));
+  .use(convert(mount('/jspm_packages', convert(serve(path.join(__dirname, 'jspm_packages'))))));
 
 // use Jade templates
 app.use(convert(views(path.join(__dirname, 'server/views'), {
-  default: 'jade'
+  extension: 'jade'
 })));
 
 // serve jspm configuration file
